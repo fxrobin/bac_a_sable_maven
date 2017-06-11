574d3bbc10024272ec51cf429ca0e29399373004
CDT RBN <CDT RBN@vmprog>
2017-06-11 16:50:36 +0200

    essais

--- demo/src/test/java/demo/DummyEntityTests.java
+++ demo/src/test/java/demo/DummyEntityTests.java
@@ -2,18 +2,28 @@ package demo;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+
 
 public class DummyEntityTests {
 
 	@Test
-	public void building() {
+	public void testBuilding() {
 		
 		DummyEntity o = DummyEntity.builder().name("Wayne").firstName("Bruce").build();
 		assertNotNull(o);
 		assertEquals("Wayne", o.getName());
 		assertEquals("Bruce", o.getFirstName());
 		
+	}
+	
+	@Test
+	public void testJava8()
+	{
+		int[] data = {12,13,14,15,16};
+		Arrays.stream(data).forEach(System.out::println);
 	}
 
 }
