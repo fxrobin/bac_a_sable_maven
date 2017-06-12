18d2fb082d730419e82f7966b7798a0a2ca49238
CDT RBN <CDT RBN@vmprog>
2017-06-12 09:25:48 +0200

    Nouveau test JAVA 8

--- demo/src/test/java/demo/DummyEntityTests.java
+++ demo/src/test/java/demo/DummyEntityTests.java
@@ -1,29 +1,34 @@
 package demo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
-
 
 public class DummyEntityTests {
 
 	@Test
 	public void testBuilding() {
-		
+
 		DummyEntity o = DummyEntity.builder().name("Wayne").firstName("Bruce").build();
 		assertNotNull(o);
 		assertEquals("Wayne", o.getName());
 		assertEquals("Bruce", o.getFirstName());
-		
+
 	}
-	
+
 	@Test
-	public void testJava8()
-	{
-		int[] data = {12,13,14,15,16};
-		Arrays.stream(data).forEach(System.out::println);
+	public void testJava8() {
+		Integer[] data = { 12, 13, 14, 15, 16 };
+		List<Integer> dataList = Arrays.stream(data).collect(Collectors.toList());
+		Integer somme = Arrays.stream(data).reduce(0, (r, v) -> r + v);
+		assertNotNull(dataList);
+		assertEquals(5, dataList.size());
+		assertEquals(70, somme.intValue());
 	}
 
 }
